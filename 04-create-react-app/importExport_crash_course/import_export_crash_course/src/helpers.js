81f45af629f5ed05cf92d210759395cb8f690f91
Levi Santos <levi-sr@outlook.com>
2020-02-24 22:51:42 -0300

    added some exercises with create-react-app

---
+++ 04-create-react-app/importExport_crash_course/import_export_crash_course/src/helpers.js
@@ -0,0 +1,17 @@
+function helpful() {
+    console.log('I did a super helpful thing');
+}
+
+function sort(){
+    console.log('All sorted');
+}
+
+function sing() {
+    console.log('LA LA LA');
+}
+
+export default helpful;
+export {
+    sort,
+    sing
+}
---
+++ 04-create-react-app/importExport_crash_course/import_export_crash_course/src/index.js
@@ -0,0 +1,5 @@
+// import helpful from './helpers';
+import helpful, { sort, sing} from './helpers'
+helpful();
+sort();
+sing();
